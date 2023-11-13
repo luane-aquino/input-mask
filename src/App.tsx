d3dde5f701f19e98b039a6e942a453066002de4d
Luane Aquino <luane.aquino.br@gmail.com>
2023-11-13 10:14:10 +0900

    get suffix width

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,115 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
+enum RadioValues {
+  money = "money",
+  points = "points",
+}
+
+type RadioValuesType = RadioValues.money | RadioValues.points;
+
+const labelText: Record<string, string> = {
+  [RadioValues.money]: "edit money",
+  [RadioValues.points]: "edit points",
+};
+
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [typeSelected, setTypeSelected] = useState<RadioValuesType>(
+    RadioValues.money,
+  );
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    setInputValue("");
+  };
+
+  const handleRadioChange = (e: any) => {
+    setTypeSelected(e.target.value);
+    setInputValue("");
+  };
+
+  const handleMoneyFormat = (value: string) => {
+    const onlyDigits = value.replace(/\D/g, "");
+    let newValue = "";
+    if (onlyDigits) {
+      newValue = `R$ ${onlyDigits}`;
+    }
+    setInputValue(newValue);
+  };
+
+  const handlePointsFormat = (value: string) => {
+    setInputValue(value);
+  };
+
+  const handleChange = (e: any) => {
+    const value = e.target.value;
+    if (typeSelected === RadioValues.money) {
+      handleMoneyFormat(value);
+    } else {
+      handlePointsFormat(value);
+    }
+  };
+
+  const getInputValueWidth = () => {
+    let canvas = document.createElement("canvas");
+    let context = canvas.getContext("2d");
+    if (context) {
+      let width = context.measureText(inputValue).width + 16;
+      return Math.ceil(width) + "px";
+    }
+    return "0";
+  };
+
   return (
     <div className="App">
-      <form>
+      <form onSubmit={handleSubmit}>
         {/* input with mask */}
         <div className="wrapper">
-          <label htmlFor="input-mask">edit money</label>
-          <input type="number" id="input-mask" />
+          <label htmlFor="input-mask">{labelText[typeSelected]}</label>
+          <input
+            type="string"
+            id="input-mask"
+            className="input"
+            value={inputValue}
+            onChange={handleChange}
+          />
+          {typeSelected === RadioValues.points && (
+            <span
+              className="suffix"
+              style={{ left: `${getInputValueWidth()}` }}
+            >
+              pts
+            </span>
+          )}
         </div>
         {/* controls */}
-        <div>
+        <fieldset>
           <div>
-            <input id="radio-money" type="radio" />
+            <input
+              id="radio-money"
+              type="radio"
+              name="maskType"
+              value={RadioValues.money}
+              onChange={handleRadioChange}
+              checked={RadioValues.money === typeSelected}
+            />
             <label htmlFor="radio-money">edit money</label>
           </div>
           <div>
-            <input id="radio-points" type="radio" />
+            <input
+              id="radio-points"
+              type="radio"
+              name="maskType"
+              value={RadioValues.points}
+              onChange={handleRadioChange}
+              checked={RadioValues.points === typeSelected}
+            />
             <label htmlFor="radio-points">edit points</label>
           </div>
-        </div>
+        </fieldset>
         {/* button */}
-        <button>confirmar</button>
+        <button disabled={inputValue.length === 0}>confirmar</button>
       </form>
     </div>
   );
